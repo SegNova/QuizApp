89a98a8ca1ba3828dad5c24b8fe5c85f01594c0e
Joshua Segura <segurajoshua@outlook.com>
2020-07-07 20:20:43 -0500

    Add new page with its css and javascript. Plus made some change to older files
    Signed-off-by: Joshua Segura <segurajoshua@outlook.com>

--- games.js
+++ games.js
@@ -1,10 +1,12 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
-let questionCounter = 0;
+var questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
@@ -38,7 +40,7 @@ let questions = [
 //constaints
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTION =  3;
+var MAX_QUESTION =  3;
 
 startGame= () => {
     questionCounter = 0;
@@ -49,10 +51,15 @@ startGame= () => {
 getNewQuestion = () => {
     
     if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTION){
+        localStorage.setItem("mostRecentScore", score);
         //go to the end page
-        return window.location.assign("*/end.html");
+        return window.location.assign("end.html");
     }
     questionCounter++;
+    progressText.innerText =   `Question ${questionCounter}/${MAX_QUESTION}`;
+    //update the progress bar
+    progressBarFull.style.width = `${questionCounter/MAX_QUESTION*100}%`;
+
     const questionIndex = Math.floor(Math.random()*availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question
@@ -60,12 +67,12 @@ getNewQuestion = () => {
     choices.forEach(choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
-    })
+    });
 
     availableQuestions.splice(questionIndex,1);
 
     acceptingAnswers = true
-}
+};
 
 choices.forEach(choice =>{
     choice.addEventListener('click',  e =>{
@@ -78,7 +85,12 @@ choices.forEach(choice =>{
         
         const classToApply = 
             selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-        selectedChoice.parentElement.classList.add(classToApply);
+        
+            if(classToApply === "correct"){
+                incrementScore(CORRECT_BONUS);
+            }
+            
+            selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
@@ -86,6 +98,10 @@ choices.forEach(choice =>{
     });
 });
 
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+};
 startGame();
 
 
